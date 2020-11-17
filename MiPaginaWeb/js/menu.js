50ddcf92d2601b14e369d35618d1acfec2a082c9
HernandezJosze <65863648+HernandezJosze@users.noreply.github.com>
2020-11-17 12:24:07 -0600

    Add files via upload

---
+++ MiPaginaWeb/js/menu.js
@@ -0,0 +1,6 @@
+let btnMenu = document.getElementById("btn-menu");
+let niv = document.getElementById("nav");
+
+btnMenu.addEventListener("click", function(){
+   niv.classList.toggle("mostrar");
+});
